d70bae68a3a73a18f5f01c8cade4d8ec9f9c55b7
Nicolò Pincelli <pincelli.n@gmail.com>
2024-08-21 18:47:28 +0200

    start styling profile card

--- js/main.js
+++ js/main.js
@@ -19,9 +19,8 @@ createApp({
       PokemonListName: [],
       PokemonSelect: null,
       PokemonSpecisUrl: null,
-      PrimaForma: "",
-      Secondaforma: "",
-      TerzaForma: "",
+      PokemonDescrizione: "",
+      CatenaEvolutiva: "",
     };
   },
 
@@ -40,7 +39,7 @@ createApp({
             this.ListAllPokemon = responses.map((response) => response.data);
             this.pokemons = this.ListAllPokemon.sort((a, b) => a.id - b.id);
             this.loading = false;
-            console.log(this.pokemons);
+            /* console.log(this.pokemons); */
           });
         }
       });
@@ -51,6 +50,7 @@ createApp({
       const Apitype = `${this.ulrApi}type/`;
       axios.get(Apitype).then((restype) => {
         this.typeList = restype.data.results;
+        console.log(this.typeList);
       });
     },
 
@@ -66,7 +66,7 @@ createApp({
         });
         this.pokemons = this.PokemonListType;
         this.Page = 1;
-        console.log(this.PokemonListType, this.ListAllPokemon);
+        /* console.log(this.PokemonListType, this.ListAllPokemon); */
       }
     },
     filterName() {
@@ -83,7 +83,7 @@ createApp({
     nextPage() {
       if (this.Page < this.pagineTotali) {
         this.Page++;
-        console.log("Next Page:", this.Page);
+        /* console.log("Next Page:", this.Page); */
       }
     },
 
@@ -91,50 +91,64 @@ createApp({
     prevPage() {
       if (this.Page > 1) {
         this.Page--;
-        console.log("Previous Page:", this.Page);
+        /* console.log("Previous Page:", this.Page); */
       }
     },
     ChoosePokemon(id) {
       this.PokemonSelect = this.pokemons.find((pokemon) => pokemon.id === id);
 
-      console.log(this.PokemonSelect, this.PokemonSelect.stats[0]);
+      console.log(this.PokemonSelect, "Parti da qui");
 
       if (this.PokemonSelect != null) {
-        console.log(this.PokemonSelect.species.url);
+        console.log(this.PokemonSelect.species);
 
         var urlSpecis = this.PokemonSelect.species.url;
 
         axios.get(urlSpecis).then((responsespiecie) => {
+          console.log(responsespiecie.data.flavor_text_entries[51].flavor_text);
+          this.PokemonDescrizione =
+            responsespiecie.data.flavor_text_entries[51].flavor_text;
           this.PokemonSpecisUrl = responsespiecie.data.evolution_chain.url;
           if (this.PokemonSpecisUrl != null) {
             axios.get(this.PokemonSpecisUrl).then((resptoevol) => {
-              console.log(
+              /*   console.log(
                 resptoevol.data.chain
-                /*  resptoevol.data.chain.species.name
+                resptoevol.data.chain.species.name
                   resptoevol.data.chain.evolves_to[0].species.name,
-                resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name */
-              );
-
+               resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name
+              );*/
+              let evoluzioni = [];
               if (resptoevol.data.chain.is_baby != true) {
-                this.PrimaForma = resptoevol.data.chain.species.name;
+                let PrimaForma = resptoevol.data.chain.species.name;
+                evoluzioni.push(PrimaForma);
                 if (resptoevol.data.chain.evolves_to[0]) {
-                  this.Secondaforma =
+                  let Secondaforma =
                     resptoevol.data.chain.evolves_to[0].species.name;
+                  evoluzioni.push(Secondaforma);
                   if (resptoevol.data.chain.evolves_to[0].evolves_to[0]) {
-                    this.TerzaForma =
-                      resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name;
+                    let TerzaForma =
+                      resptoevol.data.chain.evolves_to[0].evolves_to[0].species
+                        .name;
+                    evoluzioni.push(TerzaForma);
                   }
                 }
               } else {
-                this.PrimaForma =
+                let PrimaForma =
                   resptoevol.data.chain.evolves_to[0].species.name;
+                evoluzioni.push(PrimaForma);
                 if (resptoevol.data.chain.evolves_to[0].evolves_to[0]) {
-                  this.Secondaforma =
-                    resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name;
+                  let Secondaforma =
+                    resptoevol.data.chain.evolves_to[0].evolves_to[0].species
+                      .name;
+                  evoluzioni.push(Secondaforma);
                 }
               }
-
-              console.log(this.PrimaForma, this.Secondaforma, this.TerzaForma);
+              /* console.log(this.pokemons, evoluzioni); */
+              this.CatenaEvolutiva = this.pokemons.filter((pokemon) => {
+                /*   console.log(pokemon.name); */
+                return evoluzioni.includes(pokemon.name);
+              });
+              console.log(this.CatenaEvolutiva);
             });
           }
         });
