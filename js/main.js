3b511b2daab3c7bcf592915f960d2fd183111816
Nicolò Pincelli <pincelli.n@gmail.com>
2024-08-21 13:32:02 +0200

    add method to find evolution

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ createApp({
       Pokename: "",
       PokemonListName: [],
       PokemonSelect: null,
+      PokemonSpecisUrl: null,
+      PrimaForma: "",
+      Secondaforma: "",
+      TerzaForma: "",
     };
   },
 
@@ -92,7 +96,49 @@ createApp({
     },
     ChoosePokemon(id) {
       this.PokemonSelect = this.pokemons.find((pokemon) => pokemon.id === id);
+
       console.log(this.PokemonSelect, this.PokemonSelect.stats[0]);
+
+      if (this.PokemonSelect != null) {
+        console.log(this.PokemonSelect.species.url);
+
+        var urlSpecis = this.PokemonSelect.species.url;
+
+        axios.get(urlSpecis).then((responsespiecie) => {
+          this.PokemonSpecisUrl = responsespiecie.data.evolution_chain.url;
+          if (this.PokemonSpecisUrl != null) {
+            axios.get(this.PokemonSpecisUrl).then((resptoevol) => {
+              console.log(
+                resptoevol.data.chain
+                /*  resptoevol.data.chain.species.name
+                  resptoevol.data.chain.evolves_to[0].species.name,
+                resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name */
+              );
+
+              if (resptoevol.data.chain.is_baby != true) {
+                this.PrimaForma = resptoevol.data.chain.species.name;
+                if (resptoevol.data.chain.evolves_to[0]) {
+                  this.Secondaforma =
+                    resptoevol.data.chain.evolves_to[0].species.name;
+                  if (resptoevol.data.chain.evolves_to[0].evolves_to[0]) {
+                    this.TerzaForma =
+                      resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name;
+                  }
+                }
+              } else {
+                this.PrimaForma =
+                  resptoevol.data.chain.evolves_to[0].species.name;
+                if (resptoevol.data.chain.evolves_to[0].evolves_to[0]) {
+                  this.Secondaforma =
+                    resptoevol.data.chain.evolves_to[0].evolves_to[0].species.name;
+                }
+              }
+
+              console.log(this.PrimaForma, this.Secondaforma, this.TerzaForma);
+            });
+          }
+        });
+      }
     },
   },
 
@@ -109,7 +155,6 @@ createApp({
       return this.pokemons.slice(start, end);
     },
   },
-
   mounted() {
     this.GetAll();
     this.GetAllType();
